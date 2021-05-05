15739d3a37aaa64070e6929d48fe3ef87387d5d2
kijima <norimaking777@gmail.com>
2021-05-05 17:46:18 +0900

    Add choosing format

--- create-link.el
+++ create-link.el
@@ -7,6 +7,19 @@
 ;; ex.
 ;; <a href="https://ja.wikipedia.org/wiki/Emacs">Emacs - Wikipedia</a>
 
+(defgroup create-link nil
+  "Generate a well-formed link"
+  :group 'convenience
+  :prefix "create-link-")
+
+(defcustom create-link-default-format 'html
+  "Default format"
+  :group 'create-link
+  :type '(choice (const :tag "html" html)
+                 (const :tag "markdown" markdown)
+                 (other :tag "org" org)
+                 (other :tag "media-wiki" media-wiki)))
+
 ;; url → text
 (defcustom create-link-format-html
   "<a href='%s'>%s</a>"
@@ -26,24 +39,27 @@
   "[%s %s]"
   "media-wiki")
 
-;; get url+title test
-(defun create-link-get-eww ()
+(defun create-link-execute (format-type url title)
+  (message "Copied %s" (format format-type url title))
+  (kill-new (format format-type url title)))
+
+;; formatが順番に依存してる
+(defun create-link ()
   (interactive)
   (let ((url (plist-get eww-data :url))
         (title (plist-get eww-data :title)))
-    (create-link-copy url title)))
-
-(defun create-link-copy (url title)
-  (interactive)
-  (message "Copied %s" (format create-link-format-html url title))
-  (kill-new (format create-link-format-html url title)))
-
-;; (defun create-link-format (url title)
-;;   (format "[%s](%s)" title url))
+    (pcase create-link-default-format
+      (`html
+       (create-link-execute create-link-format-html url title))
+      (`markdown
+       (create-link-execute create-link-format-markdown title link)) ; 逆
+      (`org
+       (create-link-execute create-link-format-org url title))
+      (`media-wiki
+       (create-link-execute create-link-format-media-wiki url title))
+      ))
+  )
 
 ;; (defun title-link-w3m ()
 ;;   (interactive)
 ;; )
-
-;; URL取得
-;; format
