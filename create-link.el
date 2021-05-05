7a11b763d236a319a5cdebb4de460201f82f4eb9
kijima <norimaking777@gmail.com>
2021-05-05 16:20:37 +0900

    Add some formats

--- create-link.el
+++ create-link.el
@@ -7,21 +7,43 @@
 ;; ex.
 ;; <a href="https://ja.wikipedia.org/wiki/Emacs">Emacs - Wikipedia</a>
 
-;; (defcustom format-type :markdown)
-;; (defcustom format-html)
-;; (defcustom format-markdown)
-;; (defcustom format-org)
+;; url → text
+(defcustom create-link-format-html
+  "<a href='%s'>%s</a>"
+  "html")
+
+;; 順番が違うのでフォーマット文字列で指定できるようにする必要がある。どうしたらよいのだろう。
+;; text → url
+(defcustom create-link-format-markdown
+  "[%s](%s)"
+  "markdown")
+
+(defcustom create-link-format-org
+  "[[%s][%s]]"
+  "org")
+
+(defcustom create-link-format-media-wiki
+  "[%s %s]"
+  "media-wiki")
 
 ;; get url+title test
-(defun title-link-eww ()
+(defun create-link-get-eww ()
   (interactive)
   (let ((url (plist-get eww-data :url))
         (title (plist-get eww-data :title)))
-    (message "%s" (format (concat title " | " url)))
-    (kill-new (format (concat title " | " url)))))
+    (create-link-copy url title)))
+
+(defun create-link-copy (url title)
+  (interactive)
+  (message "Copied %s" (format create-link-format-html url title))
+  (kill-new (format create-link-format-html url title)))
+
+;; (defun create-link-format (url title)
+;;   (format "[%s](%s)" title url))
 
 ;; (defun title-link-w3m ()
-;;   (interactive))
+;;   (interactive)
+;; )
 
 ;; URL取得
 ;; format
