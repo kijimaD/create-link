87f075e6d3af56ccc56ee200417fdbce0ae70ef0
kijima <norimaking777@gmail.com>
2021-05-15 11:04:23 +0900

    Add region test

--- create-link-test.el
+++ create-link-test.el
@@ -5,15 +5,8 @@
 
 ;;; Code:
 
-(ert-deftest create-link-replace-dictionary-region-test ()
-  "If region, title region"
-  (w3m-goto-url "google.com")
-  (should (equal (create-link-replace-dictionary)
-                 '(("%url%" . "https://www.google.com/")
-                   ("%title%" . "Google")))))
-
-(ert-deftest create-link-make-format-test ()
-  "Can make format test."
+(ert-deftest create-link-make-format-buffer-test ()
+  "Each buffer can make format."
   ;; eww
   (eww "google.com")
   (should (string-match-p
@@ -33,6 +26,22 @@
     (write-file file)
     (should (string-match-p
              (format "<a href='.*/" file "'>" buffer "</a>")
+             (create-link-make-format)))
+    (delete-file file)))
+
+(ert-deftest create-link-make-format-context-test ()
+  "Each context can make format."
+  (let ((buffer "buffer")
+        (file "file")
+        (content "content"))
+    (switch-to-buffer buffer)
+    (insert content)
+    (beginning-of-buffer)
+    (mark-word)
+    (write-file file)
+
+    (should (string-match-p
+             (format "<a href='.*/" file "'>" content "</a>")
              (create-link-make-format)))
     (delete-file file)))
 
