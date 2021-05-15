e2c3c45686f80ccf613fe227d58046bf546c6c3f
kijima <norimaking777@gmail.com>
2021-05-15 10:45:18 +0900

    Add tests

--- create-link-test.el
+++ create-link-test.el
@@ -1,16 +1,41 @@
-;; Tests for create-link
+;;; create-link-test.el --- Tests for create-link
 
 (require 'ert)
 (require 'create-link)
 
-(ert-deftest create-link-make-format-test ()
-    (let ((buffer "buffer")
-          (file "file"))
-      (switch-to-buffer buffer)
-      (write-file file)
-      (should (string-match-p
-               (format "<a href='.*/" file "'>" buffer "</a>")
-               (create-link-make-format)))
-      (delete-file file)))
+;;; Code:
 
-(provide 'create-link-test)\n ;;; create-link-test.el ends here
+(ert-deftest create-link-replace-dictionary-region-test ()
+  "If region, title region"
+  (w3m-goto-url "google.com")
+  (should (equal (create-link-replace-dictionary)
+                 '(("%url%" . "https://www.google.com/")
+                   ("%title%" . "Google")))))
+
+(ert-deftest create-link-make-format-test ()
+  "Can make format test."
+  ;; eww
+  (eww "google.com")
+  (should (string-match-p
+           (format "<a href='.*google.com.*'></a>") ;; TODO: eww can't generate title when testing.
+           (create-link-make-format)))
+
+  ;; w3m
+  (w3m-goto-url "google.com")
+  (should (string-match-p
+           (format "<a href='.*google.com.*'>Google</a>")
+           (create-link-make-format)))
+
+  ;; file
+  (let ((buffer "buffer")
+        (file "file"))
+    (switch-to-buffer buffer)
+    (write-file file)
+    (should (string-match-p
+             (format "<a href='.*/" file "'>" buffer "</a>")
+             (create-link-make-format)))
+    (delete-file file)))
+
+(provide 'create-link-test)
+
+;;; create-link-test.el ends here
