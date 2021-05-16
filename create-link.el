64bce822ff5fd2d08bbc8f41fe8b51742220c50f
kijima <norimaking777@gmail.com>
2021-05-16 08:09:06 -0700

    Fix lint error

--- create-link.el
+++ create-link.el
@@ -118,15 +118,14 @@ If point is on URL, fill title with scraped one."
 
 (defun create-link-from-url ()
   "Get title from current point url."
-    (request (thing-at-point-url-at-point)
-      :parser 'buffer-string
-      :success (cl-function
-                 (lambda (&key data &allow-other-keys)
-                   (string-match "<title>\\(.*\\)</title>" data)
-                   (setq create-link-scraped-title (match-string 1 data))
-                   )))
-    (sit-for 1)
-    create-link-scraped-title)
+  (request (thing-at-point-url-at-point)
+    :parser 'buffer-string
+    :success (cl-function
+              (lambda (&key data &allow-other-keys)
+                (string-match "<title>\\(.*\\)</title>" data)
+                (setq create-link-scraped-title (match-string 1 data)))))
+  (sit-for 1)
+  create-link-scraped-title)
 
 (defun create-link-filter-title ()
   "Filter title information.
