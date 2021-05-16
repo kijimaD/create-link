2dccd58f396d92973ed4f0438727f00f0fb4b485
kijima <norimaking777@gmail.com>
2021-05-16 12:24:17 +0900

    Good looking

--- test/test-startup.sh
+++ test/test-startup.sh
@@ -2,5 +2,16 @@ git clone https://github.com/cask/cask ~/.cask
 PATH="$HOME/.cask/bin:$PATH"
 
 cask install
-cask exec emacs -Q -batch -L . -l test/create-link-test.el -f ert-run-tests-batch-and-exit
-cask exec emacs -Q -batch -L . -l create-link.el --eval "(require 'package-lint)" -f package-lint-batch-and-exit
+
+cask exec emacs \
+     -Q -batch \
+     -L . \
+     -l test/create-link-test.el \
+     -f ert-run-tests-batch-and-exit
+
+cask exec emacs \
+     -Q -batch \
+     -L . \
+     -l create-link.el \
+     --eval "(require 'package-lint)" \
+     -f package-lint-batch-and-exit
