be02161a9fce324f8766215955943857e542081e
kijima <norimaking777@gmail.com>
2021-05-17 06:38:34 -0700

    Add test message

--- test/test-startup.sh
+++ test/test-startup.sh
@@ -3,12 +3,14 @@ PATH="$HOME/.cask/bin:$PATH"
 
 cask install
 
+echo Test start...
 cask exec emacs \
      -Q -batch \
      -L . \
      -l test/create-link-test.el \
      -f ert-run-tests-batch-and-exit
 
+echo Package lint start...
 cask exec emacs \
      -Q -batch \
      -L . \
@@ -16,6 +18,7 @@ cask exec emacs \
      --eval "(require 'package-lint)" \
      -f package-lint-batch-and-exit
 
+echo Byte compile start...
 cask exec emacs \
      -Q -batch \
      -L . \
