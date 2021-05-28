68aebd2d5c39e240a89737b0eb1841e5cd2a43cf
kijima <norimaking777@gmail.com>
2021-05-28 22:31:16 +0900

    Fix fail

--- create-link.el
+++ create-link.el
@@ -265,7 +265,7 @@ If point is on URL, fill title with scraped one."
 (defun create-link-make-format (&optional format)
   "Make format link with FORMAT(optional).
 If FORMAT is not specified, use `create-link-default-format'"
-    (create-link-exec-replace (funcall (create-link-format-rule format)
+    (create-link-exec-replace (funcall (create-link-format-rule (if format format create-link-default-format))
                                        (create-link-replace-dictionary))
                               (if format format create-link-default-format)))
 
